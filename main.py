f2390ae64f1b3519a9e1bfae2452304bfa7a85d0
rafpank <portaleobs@gmail.com>
2025-03-13 08:25:42 +0100

    display of results added

--- main.py
+++ main.py
@@ -20,6 +20,45 @@ class Transaction:
         return f"{self.__class__.__name__}id={self.id!r}, description={self.description!r}, value={self.value!r})"
     
 
+    @staticmethod
+    def validate_description(description: str) -> str:
+        """Checks that the description is not empty"""
+        description = description.strip()
+        if not description:
+            raise ValueError("Błąd! Opis nie może być pusty.")
+        return description
+    
+
+    @staticmethod
+    def validate_value(value: str) -> float:
+        try:
+            value = float(value.strip())
+            if value <= 0:
+                raise ValueError("Błąd! Wartość musi być większa od 0.")
+            return value
+        except ValueError:
+            raise ValueError("Błąd! Wartość musi być liczbą większa od 0.")
+        
+
+    @classmethod
+    def from_user_input(cls) -> "Transaction":
+        """Creates a new transaction based on the data entered by the user."""
+        while True:
+            try:
+                description = cls.validate_description(input("Podaj opis: "))
+                value = cls.validate_value(input("Podaj wartość: "))
+                indicator = input("Określ czy to jest przychód czy wydatek? [p/w]").lower().strip()
+
+                if indicator == 'p':
+                    return Income(description, value)
+                elif indicator == 'w':
+                    return Expense(description, value)
+                else:
+                    return "Błąd! Wybierz 'p' dla przychodu lub 'w' dla wydatku."
+                
+            except ValueError as e:
+                print(e)
+
     def finding_transaction(transactions: List["Transaction"], user_id_choice: int) -> Optional["Transaction"]:
         for transaction in transactions:
             if transaction.id == user_id_choice:
@@ -57,6 +96,31 @@ def menu_printing() -> None:
     print("6. Zapisz zmiany w bazie danych")
     print("7. Zakończ program\n")
 
+
+def display_transactions(transactions: List[Transaction]) -> None:
+    if not transactions:
+        print("Brak zapisanych transakcji do wyświetlenia")
+        return
+    
+    incomes = [t for t in transactions if isinstance (t, Income)]
+    expenses = [t for t in transactions if isinstance(t, Expense)]
+
+    print("\n PRZYCHODY:")
+    if incomes:
+        for income in incomes:
+            print(income)
+
+    else:
+        print("Brak przychodów")
+
+    print("\n WYDATKI:")
+    if expenses:
+        for expense in expenses:
+            print(expense)
+    else:
+        print("Brak wydatków")
+                
+
     
 def file_opener(filename: str):
     try:
@@ -68,33 +132,6 @@ def file_opener(filename: str):
     return content
 
 
-def creating_transaction() -> Transaction | str:
-    while True:
-        description = input("Podaj opis: ").strip()
-        if not description:
-            print("Błąd! Opis nie może być pusty.")
-            continue
-        try:
-            value = int(input("Podaj wartość: ").strip())
-            if value <= 0:
-                print("Błąd! Wartość musi być większa od 0.")
-                continue
-        except ValueError:
-            print("Błąd! Wartość musi być liczbą.")
-            continue
-            
-        indicator_incom_or_expense = input("Określ czy to jest przychód czy wydatek? [p/w]: ").lower()
-        
-        if indicator_incom_or_expense == 'p':
-            return Income(description, value)
-        elif indicator_incom_or_expense == 'w':
-            return Expense(description, value)
-        else:
-            return "Błąd! Wybierz 'p' dla przychodu lub 'w' dla wydatku."
-
-
-
-
 def main():
     transactions = []
 
@@ -104,14 +141,19 @@ def main():
             user_choice = int(input("Wpisz odpowiednią cfrę: "))
         except ValueError:
             print("Błąd! Wpisz liczbę od 1 do 7")
+            continue
 
         if user_choice == 1:
-            new_transaction = creating_transaction()
-            if isinstance(new_transaction, Transaction):
-                transactions.append(new_transaction)
-                print(transactions)
-            else:
-                print("Niewłaściw wybór")
+            new_transaction = Transaction.from_user_input()
+            transactions.append(new_transaction)
+            print(transactions)
+
+   
+
+        elif user_choice == 3:
+            display_transactions(transactions)
+
+            
         
         elif user_choice == 7:
             break
